fb885615d7f62c7750ceb1390f570f0c259629b1
Joseph A. Wisneski IV <stray@uhstray.io>
2025-01-17 21:59:37 +0000

    Testing processing using ipynb and transform

--- transform.py
+++ transform.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sklearn.feature_extraction.text as sk_text
+import sklearn.preprocessing
 
 filepaths = list()
 
@@ -8,4 +9,11 @@ def tfidfAnalysis(dataframe):
     tfidf_frame = sk_text.TfidfTransformer(dataframe, use_idf=True, smooth_idf=True, norm='l2')
     print("Your TF-IDF matrix is " + tfidf_frame)
     
-    return dataframe, tfidf_frame
+    return tfidf_frame 
+
+def df_ez_pivot(df):
+    index=df.iloc[:,0]
+    column=df.iloc[:,1]
+    pivot_frame = df.pivot(index=index, columns=column)
+    print("Your pivot table is " + pivot_frame)
+    return pivot_frame
